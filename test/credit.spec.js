973853c770382eefc8f2198a52bf8653c820e315
Akhil <akhil9tiet@gmail.com>
2020-10-23 02:57:48 -0700

    feat: credit function test

---
+++ test/credit.spec.js
@@ -0,0 +1,22 @@
+var expect = require('chai').expect;
+const { credit } = require('../src/credit');
+
+describe('credit function tests', function () {
+	it('should reduce credit from the balance ', function () {
+		let result = [];
+		result = credit({ name: 'Tom', credit: 200 }, [{ name: 'Tom', limit: 1000, balance: 500 }]);
+		expect(JSON.stringify(result)).to.equal(JSON.stringify([{ name: 'Tom', limit: 1000, balance: 300 }]));
+	});
+
+	it('should reduce credit from the balance, even if the balance becomes negative ', function () {
+		let result = [];
+		result = credit({ name: 'Tom', credit: 2000 }, [{ name: 'Tom', limit: 1000, balance: 500 }]);
+		expect(JSON.stringify(result)).to.equal(JSON.stringify([{ name: 'Tom', limit: 1000, balance: -1500 }]));
+	});
+
+	it('should reduce credit from the balance, even if the balance becomes 0 ', function () {
+		let result = [];
+		result = credit({ name: 'Tom', credit: 500 }, [{ name: 'Tom', limit: 1000, balance: 500 }]);
+		expect(JSON.stringify(result)).to.equal(JSON.stringify([{ name: 'Tom', limit: 1000, balance: 0 }]));
+	});
+});
