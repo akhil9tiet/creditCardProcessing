e5bdc16b5f9df5974a90c74863f002a35d7b499f
Akhil <akhil9tiet@gmail.com>
2020-10-23 00:05:09 -0700

    wip: correct term use for test case

--- test/unitest.js
+++ test/unitest.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 const cardProcessing = require('../src/creditCardProcessor');
 let result = [];
 describe('end to end test', function () {
-	it('result should be correct when given complex manipulations on single user', function () {
+	it('result should show correct name and balance for complex manipulations on single user', function () {
 		result = cardProcessing.creditCardProcessor([
 			'Add Tom 4111111111111111 $1000',
 			'Charge Tom $500',
@@ -17,7 +17,39 @@ describe('end to end test', function () {
 
 	result = [];
 
-	it('result should be in lexicographical order', function () {
+	it('result should shold show correct balance, that is less than the limit for a single user', function () {
+		result = cardProcessing.creditCardProcessor([
+			'Add Tom 4111111111111111 $1000',
+			'Charge Tom $500',
+			'Charge Tom $400',
+			'Credit Tom $800',
+			'Charge Tom $300',
+			'Charge Tom $400',
+			'Charge Tom $900',
+		]);
+
+		expect(result).to.equal(JSON.stringify({ Tom: '$800' }));
+	});
+
+	result = [];
+
+	it('result should shold show correct balance, that is negative balannce for a single user', function () {
+		result = cardProcessing.creditCardProcessor([
+			'Add Tom 4111111111111111 $1000',
+			'Charge Tom $500',
+			'Charge Tom $400',
+			'Credit Tom $800',
+			'Charge Tom $300',
+			'Charge Tom $400',
+			'Credit Tom $900',
+		]);
+
+		expect(result).to.equal(JSON.stringify({ Tom: '$-100' }));
+	});
+
+	result = [];
+
+	it('result should be in lexicographical order, when there are multipe users', function () {
 		result = cardProcessing.creditCardProcessor([
 			'Add F 4111111111111111 $100',
 			'Add E 6011089621777231 $499',
@@ -32,24 +64,7 @@ describe('end to end test', function () {
 		);
 	});
 
-  result = [];
-  
-  it('result should be in lexicographical order', function () {
-		result = cardProcessing.creditCardProcessor([
-			'Add F 4111111111111111 $100',
-			'Add E 6011089621777231 $499',
-			'Add D 36387598165131 $900',
-			'Add C 4917776523231826 $200',
-			'Add B 6375739415369120 $399',
-			'Add A 5594826061176748 $600',
-		]);
-
-		expect(result).to.equal(
-			JSON.stringify({ A: 'error', B: 'error', C: 'error', D: 'error', E: 'error', F: 'error' })
-		);
-  });
-
-  result = [];
+	result = [];
 
 	it('should return 3 accounts lexicographically with balance ', function () {
 		result = cardProcessing.creditCardProcessor([
