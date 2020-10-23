edb28868fba99260b24e2543491f18308e2fa40d
Akhil <akhil9tiet@gmail.com>
2020-10-23 01:53:29 -0700

    fix: add check if limit is less than equal to zero

--- src/add.js
+++ src/add.js
@@ -1,6 +1,9 @@
 const { luhn10check } = require('./luhn10check');
 function add(input, ledger) {
 	let isCardValid = luhn10check(input.card);
+	if (input.balance <= 0) {
+		ledger.push({ name: input.name, limit: input.limit, balance: 'error' });
+	}
 	if (!isCardValid) {
 		ledger.push({ name: input.name, limit: input.limit, balance: 'error' });
 	} else {
